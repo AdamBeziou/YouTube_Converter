ab12f8bf69c98345f0aed3b3e260f4c4a29db424
Adam Beziou <adam.beziou@gmail.com>
2018-01-09 17:52:56 -0700

    Added a ListCtrl that shows files and their download status

--- YouConv.py
+++ YouConv.py
@@ -16,18 +16,29 @@ else:
 	
 class MainWindow(wx.Frame):
 	def __init__(self, parent, title):
-		wx.Frame.__init__(self, parent, title=title, size = (300,100), style = wx.DEFAULT_FRAME_STYLE)
+		wx.Frame.__init__(self, parent, title=title, size = (-1,-1), style = wx.DEFAULT_FRAME_STYLE)
 		
 		self.panel = wx.Panel(self)
-		self.desc = wx.StaticText(self.panel, label = "Enter the Video's URL:")
+		
+		self.desc = wx.StaticText(self.panel, label = "Enter A Video's URL:")
 		self.url_ctrl = wx.TextCtrl(self.panel, wx.ID_ANY, style = wx.TE_PROCESS_ENTER)
 		self.download = wx.Button(self.panel, wx.ID_ANY, "Download", size = (-1,-1))
 		self.Bind(wx.EVT_BUTTON, self.OnDownload, self.download)
 		
+		self.download_list = wx.ListCtrl(self.panel, wx.ID_ANY, style = wx.LC_REPORT)
+		self.download_list.InsertColumn(0, "Status", width = 100)
+		self.download_list.InsertColumn(1, "File", width = wx.LIST_AUTOSIZE_USEHEADER)
+		self.pos = 0
+		
 		self.sizer = wx.BoxSizer(wx.VERTICAL)
+		self.sizer.Add((0,10), 0)
 		self.sizer.Add(self.desc, 0, wx.CENTER, 5)
+		self.sizer.Add((0,5), 0)
 		self.sizer.Add(self.url_ctrl, 0, wx.CENTER, 5)
+		self.sizer.Add((0,5), 0)
 		self.sizer.Add(self.download, 0 ,wx.CENTER, 5)
+		self.sizer.Add((0,10), 0)
+		self.sizer.Add(self.download_list, 1, wx.CENTER|wx.EXPAND, 5)
 		
 		self.sizer.SetSizeHints(self.panel)
 		self.panel.SetSizerAndFit(self.sizer)
@@ -38,20 +49,30 @@ class MainWindow(wx.Frame):
 	def OnDownload(self, event):
 		test = self.url_ctrl.GetValue()
 		def TestMe(test):
+			position = self.pos
+			
 			yt = YouTube(test)
 			raw_title = yt.title
-			raw_title = raw_title.translate({ord(x): None for x in ".'"})
+			raw_title = raw_title.translate({ord(x): None for x in ".',/"})
 			
-			title = '"%s"' % (raw_title)
+			title = '\"%s\"' % (raw_title)
+			self.download_list.InsertItem(position, "Downloading...")
+			self.download_list.SetItem(position, 1, title)
 			
 			yt.streams.filter(progressive=True, file_extension = 'mp4').first().download("Videos")
 			
-			subprocess.call("ffmpeg -i Videos/%s.mp4 -vn -acodec libvorbis AudioFiles/%s.mp4" % (title, title))
-			return
+			self.download_list.SetItem(position, 0, "Converting...")
+			
+			try:
+				check = subprocess.run("ffmpeg -i Videos/%s.mp4 -vn -acodec libvorbis AudioFiles/%s.mp4" % (title, title), check = True)
+				self.download_list.SetItem(position, 0, "Complete")
+			except subprocess.CalledProcessError:
+				self.download_list.SetItem(position, 0, "Conversion Failed")
 			
 		t = threading.Thread(target = TestMe, args = (test,))
 		t.start()
 		self.url_ctrl.SetValue("")
+		self.pos += 1
 		
 app = wx.App(False)
 frame = MainWindow(None, "YouTube Video Downloader")
